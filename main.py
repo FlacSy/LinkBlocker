1766daa05ab74d078f7997a0d86cb6954493802d
FlacSy <flacsy.x@gmail.com>
2023-12-19 20:41:58 +0100

    ~

--- main.py
+++ main.py
@@ -1,56 +1,59 @@
-# main.py
 import os
+import re
 import yaml
 import logging
 from aiogram import Bot, Dispatcher, types
-from aiogram.types import ParseMode
 from aiogram.utils import executor
 from dotenv import load_dotenv
 
 load_dotenv()
 
-# Настройка логирования
+# Constants
+ADMIN_CHAT_ID = -1002021667574
+
+# Configuration logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# Загрузка токена из .env файла
+# Load token from .env file
 TOKEN = os.getenv("TELEGRAM_TOKEN")
-if TOKEN is None:
-    raise ValueError("Токен не найден. Пожалуйста, добавьте токен в файл .env")
+if not TOKEN:
+    raise ValueError("Token not found. Please add the token to the .env file.")
 
-# Загрузка списка запрещенных ссылок из файла links.yaml
+# Load forbidden links from links.yaml file
 with open("links.yaml", "r") as file:
     forbidden_links = yaml.safe_load(file)
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
-
-
 async def is_admin(message: types.Message) -> bool:
-    # Получаем информацию о чате
     chat_member = await bot.get_chat_member(message.chat.id, message.from_user.id)
-    # Проверяем, является ли пользователь администратором в текущем чате
     return chat_member.is_chat_admin()
-
 
 async def delete_message(message: types.Message):
     try:
-        # Проверка каждого сообщения на наличие запрещенных ссылок
-        for link in forbidden_links:
-            if link in message.text:
-                # Проверка, является ли отправитель сообщения администратором
+        if message.from_user.id == ADMIN_CHAT_ID:
+            return
+
+        forbidden_links_set = set(forbidden_links)
+        for link in forbidden_links_set:
+            if re.search(re.escape(link), message.text):
                 if not await is_admin(message):
-                    # Логирование информации о сообщении и ссылке
                     logger.info(
-                        f"Сообщение от {message.from_user.username} ({message.from_user.id}) в чате {message.chat.title} содержит запрещенную ссылку: {message.text}"
+                        f"Сообщение от @{message.from_user.username} в чате {message.chat.title} содержит запрещенную ссылку: {message.text}"
                     )
                     await message.delete()
-                    await bot.send_message(message.chat.id, f"@{message.from_user.username} отправил запрещенную ссылку")
-                    break  # Если найдена хотя бы одна запрещенная ссылка, прерываем проверку
-    except Exception as e:
-        logger.error(f"Произошла ошибка при обработке сообщения: {e}")
+                    await bot.send_message(
+                        message.chat.id,
+                         f"Сообщение от @{message.from_user.username} в чате {message.chat.title} содержит запрещенную ссылку: {message.text}"
+                    )
+                    break
 
+        if {"https://t.me/sepi0lscommunity/", "https://t.me/nikitasepi0l"} in message.text:
+            return
+    except Exception as e:
+        logger.error(f"An error occurred while processing the message: {e}")
 
 @dp.message_handler()
 async def echo_all(message: types.Message):
